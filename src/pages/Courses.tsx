ff64ca9f1aa15338fc810ce23e3a51a47385b0a2
DuckenClaw <sen@duckenclaw.xyz>
2025-10-17 13:30:30 +0200

    add points-block

--- src/pages/Courses.tsx
+++ src/pages/Courses.tsx
@@ -59,6 +59,39 @@ const Courses = () => {
                         </section>
                     </div>
 
+            </div>
+
+                <div className="points-container">
+
+                    <div className="no-words-container">
+                        <h3>в этом курсе<br/>нет слов: </h3>
+                        <ul>
+                            <li>вдохновение</li>
+                            <li>дисциплина</li>
+                            <li>ресурс</li>
+                            <li>муза</li>
+                            <li>проявленность</li>
+                            <li>алгоритмы</li>
+                            <li>талант</li>
+                            <li>гений</li>
+                            <li>поток</li>
+                        </ul>
+                    </div>
+
+                    <div className="yes-words-container">
+                        <h3>зато<br/>есть слова:</h3>
+                        <ul>
+                            <li>референсы</li>
+                            <li>анализ</li>
+                            <li>поструктуруализм</li>
+                            <li>нарратив</li>
+                            <li>аутентичность</li>
+                            <li>структура</li>
+                            <li>типология</li>
+                            <li>сеттинг</li>
+                        </ul>
+                    </div>
+
                 </div>
 
             </div>
